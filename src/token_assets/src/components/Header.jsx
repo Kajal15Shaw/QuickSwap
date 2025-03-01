c683e5a119df411c5b6559bf030b17f9e7363fa9
Kajal15Shaw <kajalshaw28b@gmail.com>
2025-03-01 12:13:31 +0530

    first commit

---
+++ src/token_assets/src/components/Header.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+function Header() {
+  return (
+    <header>
+      <div className="blue window" id="logo">
+        <h1>
+          <span role="img" aria-label="tap emoji">
+            🫱🏼‍🫲🏾
+          </span>
+          QuickSwap
+        </h1>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
